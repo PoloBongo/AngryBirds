76a8f5b4c7875576909effc81c637042975e575f
Arthur <earthrp.polo@gmail.com>
2025-03-26 19:15:20 +0100

    [UPDATE] adding simulation with unity physics and without unity physics

--- Assets/Scripts/Map/MapCollision.cs
+++ Assets/Scripts/Map/MapCollision.cs
@@ -7,12 +7,14 @@ public class MapCollision : MonoBehaviour
     [SerializeField] private CameraManager cameraManager;
     [SerializeField] private Transform idlePosition;
     [SerializeField] private ManageBirds manageBirds;
+    [SerializeField] private GestionLaunchBird gestionLaunchBird;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player") && slingshot.CanResetCamera)
         {
             Debug.Log(other.gameObject.name);
+            gestionLaunchBird.ClearDrawTrajectory(manageBirds.Birds[manageBirds.Index]);
             manageBirds.Index++;
             manageBirds.ManageBirdsVisibility();
             cameraManager.SwitchFollowToIdlePos();
